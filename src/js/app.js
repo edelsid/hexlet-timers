312f5da9c4f584a223fbf4dc9404e69926f78535
edelsid <eradonna@yandex.ru>
2024-12-12 04:45:02 +0300

    timers working

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,3 @@
-import Timer from "./timer";
-
 export default class App {
   constructor() {
     this.submit = document.querySelector(".timer__form");
@@ -15,7 +13,36 @@ export default class App {
   onSubmit(e) {
     e.preventDefault();
     const seconds = Number(e.target.elements[0].value);
-    const timer = new Timer(seconds);
-    this.timerArea.appendChild(timer.generate());
+    const newTimer = this.generate(seconds);
+    this.timerArea.appendChild(newTimer);
+  }
+
+  generate(seconds) {
+    const timer = document.createElement("li");
+    timer.className = "timer__item";
+    timer.innerHTML = `
+      <p class="count">${seconds}</p>
+      <button type="button" class="close">&#x2716;</button>
+    `;
+    const newCount = this.setTimer(timer, seconds);
+    timer.querySelector(".close").addEventListener("click", () => this.close(timer, newCount))
+    return timer;
+  }
+
+  setTimer(el, seconds) {
+    const countArea = el.querySelector(".count");
+    const count = setInterval(() => {
+      seconds--;
+      countArea.innerText = `${seconds}`;
+      if (seconds < 0) { 
+        this.close(el, count);
+      }
+    }, 1000);
+    return count;
+  }
+
+  close(el, count) {
+    clearInterval(count);
+    el.parentElement.closest(".timer__area").removeChild(el);
   }
 }
